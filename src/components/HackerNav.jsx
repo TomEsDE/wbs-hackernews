9d2ce40ca27e8aa035a52c19ff99787ddedb2ea9
Thomas <schmiddla@gmail.com>
2022-02-28 18:02:25 +0100

    nav search

--- src/components/HackerNav.jsx
+++ src/components/HackerNav.jsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function HackerNav() {
-  return <div>HackerNav</div>;
+export default function HackerNav({ setQuery }) {
+  const [queryInput, setQueryInput] = useState('');
+
+  function handleQueryChange({ target }) {
+    setQueryInput(target.value);
+  }
+
+  function handleSubmitQuery(event) {
+    event.preventDefault();
+    setQuery(queryInput);
+  }
+
+  return (
+    <div className="headernav-div">
+      <div className="header-logo">H</div>
+      <form
+        className="header-search-form"
+        action="#"
+        onSubmit={handleSubmitQuery}
+      >
+        <input
+          className="headernav-input-search"
+          type="text"
+          name="query"
+          value={queryInput}
+          onChange={handleQueryChange}
+          placeholder="Search for News"
+          id=""
+        />
+        <button className="headernav-button-search" onClick={handleSubmitQuery}>
+          search
+        </button>
+      </form>
+    </div>
+  );
 }
